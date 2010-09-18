e36243d42eafb4089b98141f34ab5d4fbaffa680
Mike Strong <git@thestrongfamily.org>
2010-09-18 16:27:49 -0500

    Initial Commit

---
+++ duckrace/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Duckrace::Application.config.secret_token = 'e12d9890a95923261584000b4489b144c4c9f6f9ac6581f65482ca8e7738633398901f9c317c4b1932b2416b02422fb3095795250b0bf016b396ac88a58d6105'
